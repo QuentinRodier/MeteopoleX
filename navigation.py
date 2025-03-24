47bd1212d4c9acfaf9115e4f36193f2ccebab129
DUCASTIN Vincent <ducastinv@lxktrm5>
2025-03-24 18:06:57 +0100

    Style menu

--- navigation.py
+++ navigation.py
@@ -107,13 +107,14 @@ SIDEBAR_STYLE = {
     "top": 0,
     "left": 0,
     "bottom": 0,
-    "width": "15rem",
+    "width": "20rem",
     "padding": "2rem 1rem",
     "background-color": "#f8f9fa",
+    "overflow": "scroll",
 }
 
 CONTENT_STYLE = {
-    "margin-left": "18rem",
+    "margin-left": "20rem",
     "margin-right": "2rem",
     "padding": "2rem 1rem",
 }

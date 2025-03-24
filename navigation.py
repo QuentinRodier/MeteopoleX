d4d3c25ccf5ea2cb0728264683317ced1e9163a2
DUCASTIN Vincent <ducastinv@lxktrm5>
2025-03-24 18:04:22 +0100

    Gestion css

--- navigation.py
+++ navigation.py
@@ -88,12 +88,12 @@ import radio_sondage
 
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 # ou bien https://intra.cnrm.meteo.fr/MeteopoleX/css.css
-external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css']
+#external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css']
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True, title='MeteopoleX')
-#                requests_pathname_prefix='/MeteopoleX/',
-#                routes_pathname_prefix='/')
-app.css.config.serve_locally = True
+                requests_pathname_prefix='/MeteopoleX/',
+                routes_pathname_prefix='/')
+#app.css.config.serve_locally = True
 app.scripts.config.serve_locally = True
 server = app.server
 

03826f7129294047aa39352d1f7c7ed1b52109e8
Quentin Rodier <quentin.rodier@meteo.fr>
2026-01-26 11:40:07 +0100

    Typo

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash
 
 external_stylesheets = ['assets/bootstrap.min.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True, title='MeteopoleX')
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True, title='MeteopoleX',
                 requests_pathname_prefix='/MeteopoleX/',
                 routes_pathname_prefix='/')
 # Do not move this import must stick to right after the definition of app

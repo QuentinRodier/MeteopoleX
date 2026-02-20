6b36ebca1b5480ab50c78c6da16dce2e8e5a7ad5
AnaisHennequin <anais.hennequin@meteo.fr>
2026-02-20 11:42:46 +0100

    ajout d'un load biais

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,4 +1,4 @@
-from config.models import MODELS
+from config.models import MODELS, MODELS_BIAIS
 from config.variables import VARIABLES_PLOT
 import lecture_mesoNH
 import lecture_surfex
@@ -8,22 +8,35 @@ from data.selection_data_brut_serieT import selection_data_brut_serieT
 class DataLoader:
 
     def __init__(self):
-        self.cache = {}
+        self.cache_series = {}
+        self.cache_biais = {}
 
     def load_series(self, start, end):
         key = (start, end)
 
-        if key not in self.cache:
+        if key not in self.cache_series:
             data, chart, graph = selection_data_brut_serieT(start, end)
 
-            self.cache[key] = {
+            self.cache_series[key] = {
                 "base": data,
+                #"chart": chart,
+                #"graph": graph,
                 #"meso": lecture_mesoNH.mesoNH(start, end, MODELS, VARIABLES_PLOT),
                 #"surfex": lecture_surfex.surfex(start, end, MODELS, VARIABLES_PLOT),
             }
 
-        return self.cache[key]
+        return self.cache_series[key]
 
+    def load_biais(self, start, end):
+        key = (start, end)
+
+        if key not in self.cache_biais:
+
+            self.cache_biais[key] = {
+                "meso": lecture_mesoNH.mesoNH(start, end, MODELS_BIAIS, VARIABLES_PLOT),
+                #"surfex": lecture_surfex.surfex(start, end, MODELS, VARIABLES_PLOT),
+            }
+
+        return self.cache_biais[key]
 
 data_loader = DataLoader()
-
